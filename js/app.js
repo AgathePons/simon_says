f44c7588c31ee3c1ba96a57ccccb5106a1e7327c
AgathePons <ponsagathe37@gmail.com>
2021-11-30 14:51:49 +0100

    course correction with bonus

--- js/app.js
+++ js/app.js
@@ -3,12 +3,10 @@
  * Code fourni
  */
 const app = {
-  // just a utility var to remember all the colors
   colors: ['red','green','blue','yellow'],
-  // this var will contain the sequence said by Simon
   sequence: [],
-  // compteur
   indice: 0,
+  isPlayerTurn: false,
   // HTML element needed
   messageBloc: document.getElementById('message'),
   playButton: document.getElementById('go'),
@@ -26,9 +24,8 @@ const app = {
   },
 
   bumpCell: function (color) {
-    // let's modify the syle directly
+    // modify the syle and reset the same style after a pause (150 ms)
     document.getElementById(color).style.borderWidth = '45px';
-    // and reset the same style, after a small pause (150 ms)
     setTimeout( () => {
       document.getElementById(color).style.borderWidth = '0';
     }, 150);
@@ -36,29 +33,32 @@ const app = {
 
   newGame: function () {
     app.indice = 0;
-    // start by reseting the sequence 
     app.sequence = [];
     // make it 3 times :
     for (let index = 0; index < 3; index++) {
-      // get a random number between 0 and 3
+      // get random colour and push it in sequence
       let random = Math.floor(Math.random()*4);
-      // add the corresponding color to the sequence
       app.sequence.push( app.colors[random] );
     }
     // start the "Simon Says" sequence
     app.simonSays(app.sequence);
     console.log(app.sequence);
-    app.showDelayedMessage(1350 * app.sequence.length);
+    //app.showDelayedMessage(1350 * app.sequence.length);
   },
 
   simonSays: function (sequence) {
     if (sequence && sequence.length) {
+      app.isPlayerTurn = false;
+      app.showMessage('Mémorisez la séquence');
       // after 500ms, bump the first cell
       setTimeout( app.bumpCell, 500, sequence[0] );
       // plays the rest of the sequence after a longer pause
       setTimeout( app.simonSays, 850, sequence.slice(1));
+    } else {
+      app.isPlayerTurn = true;
+      app.showMessage('Reproduisez la séquence');
+      app.timer = setTimeout(app.gameOver, 5000);
     }
-    app.showMessage('Mémorisez la séquence');
   },
   showMessage: function (message) {
     app.playButton.classList.add('hidden');
@@ -77,22 +77,25 @@ const app = {
     app.sequence.push( app.colors[random] );
     console.log(app.sequence);
     app.simonSays(app.sequence);
-    app.showDelayedMessage(500 + 850 * app.sequence.length);
+    //app.showDelayedMessage(500 + 850 * app.sequence.length);
   },
   gameOver: () => {
-    alert(`Partie terminée. Votre score est ${app.sequence.length}`);
+    alert(`Partie terminée. Votre score est ${app.sequence.length - 1}`);
     app.showButton();
     app.sequence = [];
   },
   clickPlayer: (e) => {
+    if (!app.isPlayerTurn) {
+      return;
+    }
+    clearTimeout(app.timer);
     const clickedCellId = e.target.id;
     app.bumpCell(clickedCellId);
-    
-    console.log('clic:', clickedCellId, '/', app.sequence[app.indice]);
     if (clickedCellId === app.sequence[app.indice] && app.indice + 1 < app.sequence.length) {
       console.log('bonne couleur');
       //console.log('indice:', app.indice, 'sequence:', app.sequence.length);
       app.indice++;
+      app.timer = setTimeout(app.gameOver, 5000);
     } else if (clickedCellId === app.sequence[app.indice] && app.indice + 1 === app.sequence.length) {
       //console.log('indice:', app.indice, 'sequence:', app.sequence.length);
       app.indice = 0;
@@ -103,10 +106,18 @@ const app = {
       app.gameOver();
     }
   },
+  countdown: () => {
+    window.setTimeout(console.log, 2000, 'pouet');
+    function clearAlert() {
+      window.clearTimeout(window.setTimeout(console.log, 2000, 'pouet'));
+    }
+  },
+  clearCountdown: () => {
+    window.clearTimeout(window.setTimeout(console.log, 2000, 'pouet'));
+  },
 
   init: () => {
     app.drawCells();
-    // listen click on the "go" button
     app.playButton.addEventListener('click', app.newGame);
     app.playground.addEventListener('click', app.clickPlayer);
   },
